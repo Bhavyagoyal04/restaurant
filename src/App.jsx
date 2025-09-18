888d68d69165218139c2655ba6ee315e9c21b30c
Bhavya Goyal <bhavyagoyal30122005@gmail.com>
2025-09-18 23:55:14 +0530

    OK

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
                 <Route path="/restaurant/:id" element={<RestaurantDetail />} />
               </Route>
               
-              <Route path="*" element={<Navigate to="/login" />} />
+              <Route path="*" element={<Navigate to="/login" replace />} />
             </Routes>
           </main>
         </div>
